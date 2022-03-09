ef6e5d3b01e978a99f1b3b2985da92ead0d437af
giorgios17 <giosur@live.it>
2022-03-09 14:11:05 +0100

    dice game done

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
 
+const player = Math.floor((Math.random() * 6) + 1)
+const computer = Math.floor((Math.random() * 6) + 1)
+console.log('Hai lanciato il dado ed è uscito ' + player);
+console.log('Il computer ha lanciato il dado ed è uscito ' + computer);
+
+if (player > computer) {
+    console.log('Complimenti hai vinto!')
+}
+else if (computer > player) {
+    console.log('Hai perso! Riprova, sarai più fortunato.')
+}
+else {
+    console.log('Hai pareggiato!')
+}
